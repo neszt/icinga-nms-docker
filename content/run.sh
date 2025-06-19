140e394b9009499ef74a204fbab7c60ba550d917
Neszt Tibor <tibor@neszt.hu>
2025-06-19 16:41:53 +0200

    GIT_AUTOPULL implemented

--- content/run.sh
+++ content/run.sh
@@ -91,4 +91,10 @@ cd /config && touch nag.xml && gen_config.sh 1
 
 cat hosts 2>/dev/null >> /etc/hosts
 
+#
+# Autopull
+#
+
+[ -z "${GIT_AUTOPULL}" ] && rm -rf /etc/cron.d/autopull
+
 exec runsvdir /etc/service
---
+++ content/usr/local/bin/autopull.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e -x
+
+cd /config
+
+git remote update
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Autopull - Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Autopull - Need to pull"
+    git pull
+    gen_config.sh
+else
+    echo "Autopull - Unknown case"
+fi
